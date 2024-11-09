1253550b0f902e3a79099011cace1fddbd5ee1c4
Shaamallow <eyal.benaroche@gmail.com>
2024-11-09 18:56:21 +0100

    add repo utils
    - lint workflow
    - pip pyproject (wiht dep, black, isort)
    - License (AGPL)

--- src/operators/__init__.py
+++ src/operators/__init__.py
@@ -1,10 +1,13 @@
 from .generation_operators import generation_register, generation_unregister
-from .history_collection_operators import (history_collection_register,
-                                           history_collection_unregister)
-from .image_render_operators import (image_render_register,
-                                     image_render_unregister)
-from .mesh_collection_operators import (mesh_collection_register,
-                                        mesh_collection_unregister)
+from .history_collection_operators import (
+    history_collection_register,
+    history_collection_unregister,
+)
+from .image_render_operators import image_render_register, image_render_unregister
+from .mesh_collection_operators import (
+    mesh_collection_register,
+    mesh_collection_unregister,
+)
 
 
 def register():
